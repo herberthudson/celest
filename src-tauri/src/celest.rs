64fd98b6e41182ddec9b70bc19db54e8f459e646
Herbert Hudson <herberthudson@MacBook-Air-de-Herbert.local>
2023-09-07 23:01:16 -0300

    chore: moving constants to separated file

--- src-tauri/src/celest.rs
+++ src-tauri/src/celest.rs
@@ -1,5 +1,9 @@
 use serde::{Deserialize, Serialize};
 
+pub const JOURNAL_LOG: &str = r"Journal(Alpha|Beta)?\.[0-9]{2,4}(-)?[0-9]{2}(-)?[0-9]{2}(T)?[0-9]{2}[0-9]{2}[0-9]{2}\.[0-9]{2}\.log$";
+pub const ED_FILES: &str =
+    r"(Cargo|Market|ModulesInfo|NavRoute|Outfitting|ShipLoker|Shipyard|Status)\.json$";
+
 #[derive(Debug, Deserialize)]
 pub struct EliteDangerousLogEvent {
     timestamp: String,
